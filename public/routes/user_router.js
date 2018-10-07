babffca42fbe51f3f12a682b8c49bd4fe1c005eb
Aidan <14ac104@queensu.ca>
2018-10-07 18:20:29 -0400

    middleware routing for collecting information about the user

---
+++ public/routes/user_router.js
@@ -0,0 +1,85 @@
+require('dotenv').config();
+const express = require('express'),
+      router = express.Router();
+      request = require('request'); // "Request" library
+      cors = require('cors');
+      querystring = require('querystring');
+      cookieParser = require('cookie-parser');
+
+router.use(function timeLog (req, res, next) {
+  console.log('Time: ', Date.now());
+  next();
+});
+
+// called when /user endpoint is hit
+// get users saved albums and look at what genres they like
+router.get('/',(req, res)=>{var access_token = req.query.access_token;
+  // console.log('here is token: ' + access_token);
+  
+  // var playSong = {
+  //     url: 'https://api.spotify.com/v1/me/player/play',
+  //     headers: {
+  //       'Authorization': 'Bearer ' + access_token
+  //     },
+  //     json: true
+  //   };
+
+  // req.put(playSong, function(req, res){
+  //   if (res){
+  //     console.log('should be playing song');
+  //   }
+  // });
+
+
+  // var flag = true;
+  // var genres = new Map();
+
+  // while (flag) {
+	var offset = 0;
+	var getSavedAlbums = {
+		url: 'https://api.spotify.com/v1/me/albums',
+		// qs: {
+	//      limit: 50,
+	//      offset: offset
+	//  },
+	headers: {
+	   'Authorization': 'Bearer ' + access_token
+	},
+	json: true
+	};
+
+	var i;
+  request.get(getSavedAlbums, (err, response, body)=>{
+  	if (err) console.log(err);
+  	else {
+  		for(i = 0; i < body.items.length; i++){
+			
+			var getArtistGenre = {
+			  	url: 'https://api.spotify.com/v1/artists/' + body.items[i].album.artists[0].id,
+			  	// qs: {
+			   //      limit: 50,
+			   //      offset: offset
+			   //  },
+				headers: {
+				   'Authorization': 'Bearer ' + access_token
+				},
+				json: true
+			};
+
+  			request.get(getArtistGenre, (err, response, body)=>{
+  				if (err) console.log(err);
+  				else {
+  					console.log(body.genres);
+  				}
+  			});
+  		}
+  	}
+  });
+  // }
+
+
+  res.sendFile("test.html", {"root": __dirname + '/../'});
+});
+
+
+module.exports = router;
